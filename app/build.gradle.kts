7fd67c1c437d0b63361634ff08704fa61235feef
Patricia S. Gracia Artero <61123929+ptrcia@users.noreply.github.com>
2025-06-10 11:36:13 +0200

    version2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.patriciagracia.catclicker"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
